a2b4722cfb6c59815eac0e6c513c98d4d6650c2c
aziz <aziz@git.com>
2023-05-17 17:45:19 +0100

    test

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,7 @@
    <div>
         <main class="flex-1 overflow-x-hidden overflow-y-auto bg-gray-200">
             <div class="container mx-auto px-6 py-8">
-
+<h1>change</h1>
                 
                 <h3 class="text-gray-700 text-3xl font-medium">Welcome : {{ auth()->user()->name }}</h3>                
 
