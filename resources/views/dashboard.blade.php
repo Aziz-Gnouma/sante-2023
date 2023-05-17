250a2520dc08e16fcda3d2b780a8e6ecc9ff80ef
aziz <aziz@git.com>
2023-05-17 18:41:03 +0100

    step aziz

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,10 +1,15 @@
 
+@php
+    $totalUsers = DB::table('frontusers')->count();
+    $totalAdmins = DB::table('users')->count();
+
+@endphp
 
 <x-app-layout>
    <div>
         <main class="flex-1 overflow-x-hidden overflow-y-auto bg-gray-200">
             <div class="container mx-auto px-6 py-8">
-
+<h1>change</h1>
                 
                 <h3 class="text-gray-700 text-3xl font-medium">Welcome : {{ auth()->user()->name }}</h3>                
 
@@ -15,15 +20,17 @@
                 </b> </p>
 
             </div>
+
+
             <div class="container-fluid pt-4 px-4">
-                <div class="row g-4">
+                <div style="color:white;" class="row g-4">
                     <div class="col-sm-6 col-xl-3">
                         <div class="bg-secondary rounded d-flex align-items-center justify-content-between p-4">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bar-chart-fill" viewBox="0 0 16 16">
   <path d="M1 11a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1v-3zm5-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v7a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7zm5-5a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1V2z"/>
 </svg>                            <div class="ms-3">
-                                <p class="mb-2">Today Sale</p>
-                                <h6 class="mb-0">$1234</h6>
+                            <p style="font-weight: bolder; color:black ;" class="mb-2">Total number of Consultasion</p>
+                            <h6 style="font-weight: bolder; color:white ; margin-left:50%;" class="mb-0">{{ $totalUsers }}</h6>
                             </div>
                         </div>
                     </div>
@@ -33,8 +40,8 @@
   <path d="m10.495 6.92 1.278-.619a.483.483 0 0 0 .126-.782c-.252-.244-.682-.139-.932.107-.23.226-.513.373-.816.53l-.102.054c-.338.178-.264.626.1.736a.476.476 0 0 0 .346-.027ZM7.741 9.808V9.78a.413.413 0 1 1 .783.183l-.22.443a.602.602 0 0 1-.12.167l-.193.185a.36.36 0 1 1-.5-.516l.112-.108a.453.453 0 0 0 .138-.326ZM5.672 12.5l.482.233A.386.386 0 1 0 6.32 12h-.416a.702.702 0 0 1-.419-.139l-.277-.206a.302.302 0 1 0-.298.52l.761.325Z"/>
   <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0ZM1.612 10.867l.756-1.288a1 1 0 0 1 1.545-.225l1.074 1.005a.986.986 0 0 0 1.36-.011l.038-.037a.882.882 0 0 0 .26-.755c-.075-.548.37-1.033.92-1.099.728-.086 1.587-.324 1.728-.957.086-.386-.114-.83-.361-1.2-.207-.312 0-.8.374-.8.123 0 .24-.055.318-.15l.393-.474c.196-.237.491-.368.797-.403.554-.064 1.407-.277 1.583-.973.098-.391-.192-.634-.484-.88-.254-.212-.51-.426-.515-.741a6.998 6.998 0 0 1 3.425 7.692 1.015 1.015 0 0 0-.087-.063l-.316-.204a1 1 0 0 0-.977-.06l-.169.082a1 1 0 0 1-.741.051l-1.021-.329A1 1 0 0 0 11.205 9h-.165a1 1 0 0 0-.945.674l-.172.499a1 1 0 0 1-.404.514l-.802.518a1 1 0 0 0-.458.84v.455a1 1 0 0 0 1 1h.257a1 1 0 0 1 .542.16l.762.49a.998.998 0 0 0 .283.126 7.001 7.001 0 0 1-9.49-3.409Z"/>
 </svg>                                                    <div class="ms-3">
-                                <p class="mb-2">Total Sale</p>
-                                <h6 class="mb-0">$1234</h6>
+                                <p style="font-weight: bolder; color:black ;" class="mb-2">Total number of users</p>
+                                <h6 style="font-weight: bolder; color:white ; margin-left:50%;" class="mb-0">{{ $totalUsers }}</h6>
                             </div>
                         </div>
                     </div>
@@ -44,8 +51,8 @@
   <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7Zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm-5.784 6A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1h4.216ZM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z"/>
 </svg>
                             <div class="ms-3">
-                                <p class="mb-2">Todal Users</p>
-                                <h6 class="mb-0">$1234</h6>
+                            <p style="font-weight: bolder; color:black ;" class="mb-2">Total number of admins</p>
+                            <h6 style="font-weight: bolder; color:white ; margin-left:50%;" class="mb-0">{{ $totalAdmins }}</h6>
                             </div>
                         </div>
                     </div>
@@ -56,8 +63,8 @@
   <path d="M8 14.5c.5 0 .999-.046 1.479-.139L8.4 15.8a.5.5 0 0 1-.8 0l-1.079-1.439c.48.093.98.139 1.479.139Z"/>
 </svg>
                             <div class="ms-3">
-                                <p class="mb-2">Total Revenue</p>
-                                <h6 class="mb-0">$1234</h6>
+                            <p style="font-weight: bolder; color:black ;" class="mb-2">Total number of Shows</p>
+                            <h6 style="font-weight: bolder; color:white ; margin-left:50%;" class="mb-0">{{ $totalUsers }}</h6>
                             </div>
                         </div>
                     </div>
@@ -66,18 +73,18 @@
             <div class="container-fluid pt-4 px-4">
                 <div class="row g-4">
                     <div class="col-sm-12 col-xl-6">
-                        <div class="bg-secondary text-center rounded p-4">
+                        <div  style="background:linear-gradient(180deg, rgba(222,225,230,1) 0%, rgba(0,5,28,1) 0%, rgba(2,22,108,1) 33%, rgba(1,10,42,1) 100%) ;" class="bg-secondary text-center rounded p-4">
                             <div class="d-flex align-items-center justify-content-between mb-4">
-                                <h6 class="mb-0">Worldwide Sales</h6>
-                                <a href="">Show All</a>
+                                <h6 style="color:white;" class="mb-0">Worldwide Sales</h6>
+                                <a style="color:white;" href="">Show All</a>
                             </div>
                             <canvas id="worldwide-sales" style="display: block; box-sizing: border-box; height: 356.8px; width: 715.2px;" width="894" height="446"></canvas>                        </div>
                     </div>
                     <div class="col-sm-12 col-xl-6">
-                        <div class="bg-secondary text-center rounded p-4">
+                        <div style="background:linear-gradient(180deg, rgba(222,225,230,1) 0%, rgba(0,5,28,1) 0%, rgba(2,22,108,1) 33%, rgba(1,10,42,1) 100%) ;" class=" text-center rounded p-4">
                             <div class="d-flex align-items-center justify-content-between mb-4">
-                                <h6 class="mb-0">Salse & Revenue</h6>
-                                <a href="">Show All</a>
+                                <h6 style="color:white;" class="mb-0">Salse & Revenue</h6>
+                                <a style="color:white;" href="">Show All</a>
                             </div>
                             <canvas id="salse-revenue"></canvas>
                         </div>
@@ -88,3 +95,5 @@
     </div>
 </div>
 </x-app-layout>
+
+
