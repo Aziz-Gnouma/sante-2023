5d2a51c9e2a67d91cc4a9b0b37415423cc4dc0b2
aziz <aziz@git.com>
2023-05-17 17:03:22 +0100

    change

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,7 +8,6 @@
 
         <!-- Validation Errors -->
         <x-auth-validation-errors class="mb-4" :errors="$errors" />
-
         <form method="POST" action="{{ route('register') }}">
             @csrf
 
